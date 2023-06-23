37afdeabbb01a940cd78b9db7dd0666fd6959b23
bwrosen20 <bwrosen20@gmail.com>
2023-06-23 13:07:42 -0400

    Testing google book api

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react'
 
 function App() {
+
+  const [books,setBooks]=useState([])
+
+  useEffect(()=>{
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=intitle:percy jackson&orderBy=relevance`)
+    .then(r=>r.json())
+    .then(data=>{
+      setBooks(data.items.filter((book)=>(book.volumeInfo.ratingsCount>0) && (book.volumeInfo.language==="en")).sort((a,b)=>b.volumeInfo.ratingsCount - a.volumeInfo.ratingsCount))
+    })
+
+    
+   
+  },[])
+
+  console.log(books)
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {books.map((book)=>(
+        <h1>{book.volumeInfo.title}</h1>
+      ))}
+      
     </div>
   );
 }
