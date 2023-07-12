196e1375764f6a783aebb2e793e2903db73f4209
bwrosen20 <bwrosen20@gmail.com>
2023-07-12 09:56:04 -0400

    Production storage

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,14 @@ class User < ApplicationRecord
      has_one_attached :profile_image
 
      validates :name, presence: :true
+     validate :acceptable_image
      validates :name, uniqueness: :true
      validates :password, presence: :true, confirmation: true, on: :create
      validates :password, length: {minimum: 8}, confirmation: true, on: :create
+
+     def acceptable_image
+          return unless profile_image.attached?
+     end
 
 end
 
