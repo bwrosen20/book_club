3df9425b32fd37bc0b199122db8bf0663d48b349
bwrosen20 <bwrosen20@gmail.com>
2023-07-13 06:27:34 -0400

    Seed data

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
      has_secure_password
      has_many :reviews, dependent: :destroy
      has_many :books, through: :reviews
-     has_one_attached :profile_image
+     has_one_attached :profile_image, dependend: :destroy
 
      validates :name, presence: :true
      validate :acceptable_image
