394fd4561cbbfab940118e61010269b7be102e67
bwrosen20 <bwrosen20@gmail.com>
2023-07-13 06:30:16 -0400

    Clear active storage on seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
      has_secure_password
      has_many :reviews, dependent: :destroy
      has_many :books, through: :reviews
-     has_one_attached :profile_image, dependend: :destroy
+     has_one_attached :profile_image, dependent: :destroy
 
      validates :name, presence: :true
      validate :acceptable_image
