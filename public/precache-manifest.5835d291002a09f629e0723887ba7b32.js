5487bb2e7c57d1290a37a90ff0eec4b7b965c6fd
bwrosen20 <bwrosen20@gmail.com>
2023-10-21 08:31:14 -0400

    Mobile navbar

---
+++ public/precache-manifest.5835d291002a09f629e0723887ba7b32.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6456f017c80d6d917272",
+    "url": "/static/css/main.023deb56.chunk.css"
+  },
+  {
+    "revision": "6456f017c80d6d917272",
+    "url": "/static/js/main.6456f017.chunk.js"
+  },
+  {
+    "revision": "c44f1508a1ad007ec3fa",
+    "url": "/static/js/1.c44f1508.chunk.js"
+  },
+  {
+    "revision": "e7777ff626be2dba596a",
+    "url": "/static/js/2.e7777ff6.chunk.js"
+  },
+  {
+    "revision": "6ad7ffebbf5ca260e564",
+    "url": "/static/js/runtime~main.6ad7ffeb.js"
+  },
+  {
+    "revision": "673f9a9a586f5a101f07f253cc58ce66",
+    "url": "/static/media/MUSTACHE.673f9a9a.png"
+  },
+  {
+    "revision": "1312eac15c24190e2d865ac1701f8736",
+    "url": "/static/media/bookBackground.1312eac1.png"
+  },
+  {
+    "revision": "bdfd14538fef4d893bab543656a9c507",
+    "url": "/index.html"
+  }
+];
